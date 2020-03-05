69862c922852e5827b6679d903226b1f7947065c
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2020-03-04 22:30:19 -0800

    can't override File.toPath() yet, Path is minSDK 26 on Android

--- core/src/com/biglybt/core/util/FileUtil.java
+++ core/src/com/biglybt/core/util/FileUtil.java
@@ -3821,11 +3821,14 @@ public class FileUtil {
 			return super.setWritable(writable, ownerOnly);
 		}
 		
+/* Path is minSDK 26 on Android
 		@Override
 		public Path toPath(){
 			Debug.out( "!" );
 			return( super.toPath());
 		}
+		
+ */
 		
 		@Override
 		public URI toURI(){
